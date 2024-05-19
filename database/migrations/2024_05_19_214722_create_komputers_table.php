7ae14ccf38e3898f5f12cbf2519439a659a1dd1f
FadhaeilRizkiMahwendi <211220095@unmuhpnk.ac.id>
2024-05-20 06:55:23 +0700

    first commit

---
+++ database/migrations/2024_05_19_214722_create_komputers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('komputers', function (Blueprint $table) {
+
+            $table->enum('merek',['asus','acer','dell','lain'])->nullable();
+            $table->string('kelengkapan', length: 10)->primary();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('komputers');
+    }
+};
